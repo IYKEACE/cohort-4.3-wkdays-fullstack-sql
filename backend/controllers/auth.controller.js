20e92668ac980679f02f112b86eaec399b39c3f7
iykeace <ikechukwuu338@gmail.com>
2025-10-14 13:19:47 +0100

    created login and updated the register page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import { pool } from "../database/connection.js";
-import { hashPassword } from "../utils/utilities.js";
-import { registerUser } from "../database/queries/sql.js";
+import {
+  generateToken,
+  hashPassword,
+  verifyPassword,
+} from "../utils/utilities.js";
+import { registerUser, findEmail } from "../database/queries/sql.js";
+
 // Full CRUD application
 
 /**
@@ -12,8 +17,22 @@ export const register = async (req, res) => {
     const { first_name, last_name, email, role, password } = req.body;
     const hashedPassword = hashPassword(password);
     const values = [first_name, last_name, email, role, hashedPassword];
+    const { rows } = await pool.query(findEmail, [email]);
+    console.log(rows[0]);
+    if (rows[0]) {
+      res.status(401).json({
+        message: "user already exists, kindly login",
+      });
+    }
     const result = await pool.query(registerUser, values);
-    res.status(201).json(result.rows[0]);
+    const firstname = result.rows[0].first_name;
+    console.log("RESULT", result);
+    res.status(201).json({
+      status: "success",
+      data: {
+        message: `Welcome aboard ${firstname}, your account has been created successfully`,
+      },
+    });
   } catch (error) {
     res.status(500).json({
       error: error.message,
@@ -25,9 +44,58 @@ export const register = async (req, res) => {
  * Login
  */
 
+export const login = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const { rows } = await pool.query(findEmail, [email]);
+    const { id, role, first_name } = rows[0];
+    console.log(id, role);
+
+    // check password match
+    const checkIfPasswordMatch = verifyPassword(
+      rows[0].password,
+      req.body.password
+    );
+
+    // if pass does not match
+    if (!checkIfPasswordMatch) {
+      return res.status(401).json({
+        error:
+          "Invalid User credentials, Either email or password is incorrect",
+      });
+    }
+
+    //generate token
+    const token = generateToken(
+      {
+        id,
+        role,
+      },
+      process.env.JWT_SECRET_KEY, { expiresIn: "7d" }
+    );
+    return res.status(200).json({
+      status: "success",
+      id,
+      token,
+      message: `Welcome back ${first_name}, you have logged in successfully`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+
 /**
  * Forgot password
  */
+
+// export const forgetPassword = async (req, res) => {
+//   try {
+//     const { email } = req.body;
+//   } catch (error) {}
+// };
 
 /**
  * Reset password
--- backend/database/queries/sql.js
+++ backend/database/queries/sql.js
@@ -1,2 +1,8 @@
 export const registerUser = 
 "INSERT INTO users (first_name, last_name, email, role, password) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+export const findEmail = "SELECT * FROM users WHERE email = $1";
+
+export const findIfEmailExist = "SELECT * FROM users  WHERE email = $1";
+
+
+// SELECT ALL WHERE EMAIL = $1
