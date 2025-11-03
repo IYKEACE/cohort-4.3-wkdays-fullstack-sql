f533c225823fb3b8a042150d7afd27d4a4efa995
iykeace <ikechukwuu338@gmail.com>
2025-11-03 11:08:09 +0100

    Fix SSL connection issues for render postgres 4

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -5,23 +5,28 @@ dotenv.config();
 
 const { Pool } = pg;
 
+const connectionString = process.env.DATABASE_URL;
+
+// detect environment
+const isRender =
+  process.env.RENDER === "true" || process.env.NODE_ENV === "production";
+
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:
-  process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  connectionString,
+  ssl: isRender
+    ? { require: true, rejectUnauthorized: false } // for Render
+    : false, // for local dev
 });
 
-// create connection to db
 const createConnectionToDB = async () => {
   try {
     const client = await pool.connect();
-    console.log("Connected to the database successfully.");
+    console.log("✅ Connected to the database successfully.");
     return client;
   } catch (error) {
-    console.error("Error connecting to the database:", error);
+    console.error("❌ Error connecting to the database:", error);
     throw error;
   }
 };
+
 export default createConnectionToDB;
